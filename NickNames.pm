ee308807e05461b5172227bf63d02e7089bd8781
Paul Miller <jettero@cpan.org>
2009-04-30 07:03:22 -0400

    thinking outloud

---
+++ NickNames.pm
@@ -0,0 +1,41 @@
+
+package Term::ANSIColor::ExtraColors;
+
+use strict;
+use warnings;
+use base 'Term::ANSIColors';
+
+our %NICKNAMES = (
+    red         => "31",
+    blood       => "31",
+    umber       => "1;31",
+    blue        => "34",
+    sky         => "1;34",
+    ocean       => "36",
+    cyan        => "1;36",
+    green       => "32",
+    lime        => "1;32",
+    orange      => "33",
+    brown       => "33",
+    yellow      => "1;33",
+    magenta     => "35",
+    purple      => "35",
+    violet      => "1;35",
+    boldpurple  => "1;35",
+    boldmagenta => "1;35",
+    black       => "1;30",
+    white       => "1;37",
+    dire        => "1;33;41",
+    # 40 set black background
+    # 41 set red background
+    # 42 set green background
+    # 43 set brown background
+    # 44 set blue background
+    # 45 set magenta background
+    # 46 set cyan background
+    # 47 set white background
+    # 49 set default background color
+)
+
+
+"true";
