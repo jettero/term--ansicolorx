44c6c4356ea315b21c3ed47b68dfa1e1774c18e7
Paul Miller <jettero@cpan.org>
2009-05-01 12:10:21 -0400

    I had some things in here I shouldn't have

--- ColorNicknames.pm
+++ ColorNicknames.pm
@@ -8,24 +8,21 @@ use Term::ANSIColor;
 our $VERSION = '2.718'; # 2.71828183 # version approaches e
 
 our %NICKNAMES = (
-    red         => "31",
     blood       => "31",
     umber       => "1;31",
-    blue        => "34",
     sky         => "1;34",
     ocean       => "36",
+    lightblue   => "36",
     cyan        => "1;36",
-    green       => "32",
     lime        => "1;32",
     orange      => "33",
     brown       => "33",
     yellow      => "1;33",
-    magenta     => "35",
     purple      => "35",
     violet      => "1;35",
-    boldpurple  => "1;35",
-    boldmagenta => "1;35",
     black       => "1;30",
+    grey        => "37",
+    gray        => "37",
     white       => "1;37",
     dire        => "1;33;41",
 );
