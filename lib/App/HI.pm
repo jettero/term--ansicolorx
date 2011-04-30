c618a3c8ea9c54e0dea7e1a9b77b8b3a5f86f329
Paul Miller <jettero@cpan.org>
2011-04-30 07:04:29 -0400

    make a placeholder so cpan indexes the handy hi command

---
+++ lib/App/HI.pm
@@ -0,0 +1,17 @@
+
+package App::HI;
+our $VERSION = '2.7184';
+
+__END__
+
+=head1 NAME
+
+App::HI - highlight things in a stream of output
+
+=head1 SYNOPSIS
+
+This is just a placeholder for the command line app.
+
+=head1 SEE ALSO
+
+perl(1), hi(1), L<Term::ANSIColor>, L<Term::ANSIColorx::AutoFilterFH>, L<Term::ANSIColorx::ColorNicknames>
