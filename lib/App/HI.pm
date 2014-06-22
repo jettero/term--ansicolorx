79bc44c8e9d49a5f9f4cc85e1b8be26354289e62
Paul Miller <jettero@corky.vhb>
2014-06-22 10:20:17 -0400

    move hi to App::HI

--- lib/App/HI.pm
+++ lib/App/HI.pm
@@ -1,6 +1,34 @@
 
 package App::HI;
+
+use strict;
+use Text::Table;
+
 our $VERSION = '2.7187';
+
+sub fire_filter {
+    my $truncate = shift;
+    my $no_extra = shift;
+
+    unless( $no_extra ) {
+        eval q { use Term::ANSIColorx::ColorNicknames; 1 }
+        or die $@;
+    }
+
+    eval q { use Term::ANSIColorx::AutoFilterFH qw(filtered_handle); 1 }
+    or die $@;
+
+    my $newstdout = filtered_handle(\*STDOUT, @ARGV);
+    $| = 1; my $oldstdout = select $newstdout; $|=1;
+    $newstdout->set_truncate($truncate) if $truncate;
+
+    binmode $newstdout, ':utf8';
+    binmode STDIN,      ':utf8';
+
+    while(<STDIN>) {
+        print
+    }
+}
 
 __END__
 
