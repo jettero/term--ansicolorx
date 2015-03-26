8962bef8631b0f0b3ccedbcc1da7135182e5cc09
Paul Miller <jettero@corky.vhb>
2015-03-26 10:33:07 -0400

    why does t/07 fail?

--- new-version.sh
+++ new-version.sh
@@ -2,14 +2,14 @@
 
 git clean -dfx
 
-files=( $(grep '\$VERSION\s*=\s*.[0-9.]+.;' -Erl) )
+files=( $(grep '\$VERSION\s*=\s*.[0-9._]+.;' -Erl) )
 
 echo
 echo files to update:
-grep -Er '\$VERSION\s*=\s*.[0-9.]+.;' --color=always
+grep -Er '\$VERSION\s*=\s*.[0-9._]+.;' --color=always
 echo
 
-DEFAULT=$( grep -E '\$VERSION\s*=\s*.[0-9.]+.;' < "${files[0]}" | sed -e s/[^0-9.]//g )
+DEFAULT=$( grep -E '\$VERSION\s*=\s*.[0-9._]+.;' < "${files[0]}" | sed -e s/[^0-9._]//g )
 
 read -ep "new version: " -i $DEFAULT VERSION
 
